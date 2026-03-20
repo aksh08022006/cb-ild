918e5115b8d5f7c4939975f530e72da7689e5a2d
Aksh Kaushik <aksh08022006@gmail.com>
2026-03-20 17:37:55 +0530

    refactor: Remove dashboard module and simplify routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -12,49 +12,26 @@ export const routes: Routes = [
     loadComponent: () => import('./core/components/layout/layout.component').then(m => m.LayoutComponent),
     canActivate: [authGuard],
     children: [
-      { 
-        path: '', 
-        loadComponent: () => import('./modules/dashboard/dashboard.component').then(m => m.DashboardComponent)
-      },
+      { path: '', redirectTo: 'clients', pathMatch: 'full' },
       {
         path: 'clients',
         loadComponent: () => import('./modules/clients/clients.component').then(m => m.ClientsComponent)
-      },
-      {
-        path: 'kyc-completeness',
-        loadComponent: () => import('./modules/kyc-completeness/kyc-completeness.component').then(m => m.KycCompletenessComponent)
       },
       {
         path: 'kyc/:clientId',
         loadComponent: () => import('./modules/kyc-completeness/kyc-completeness.component').then(m => m.KycCompletenessComponent)
       },
       {
-        path: 'submission-dashboard',
-        loadComponent: () => import('./modules/submission-dashboard/submission-dashboard.component').then(m => m.SubmissionDashboardComponent)
-      },
-      {
         path: 'submissions',
         loadComponent: () => import('./modules/submission-dashboard/submission-dashboard.component').then(m => m.SubmissionDashboardComponent)
-      },
-      {
-        path: 'bureau-monitor',
-        loadComponent: () => import('./modules/bureau-monitor/bureau-monitor.component').then(m => m.BureauMonitorComponent)
       },
       {
         path: 'bureau/:clientId',
         loadComponent: () => import('./modules/bureau-monitor/bureau-monitor.component').then(m => m.BureauMonitorComponent)
       },
       {
-        path: 'data-insights',
-        loadComponent: () => import('./modules/data-insights/data-insights.component').then(m => m.DataInsightsComponent)
-      },
-      {
         path: 'insights/:clientId',
         loadComponent: () => import('./modules/data-insights/data-insights.component').then(m => m.DataInsightsComponent)
-      },
-      {
-        path: 'dispute-resolution',
-        loadComponent: () => import('./modules/dispute-resolution/dispute-resolution.component').then(m => m.DisputeResolutionComponent)
       },
       {
         path: 'disputes',
