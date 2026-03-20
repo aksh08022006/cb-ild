262858c416ed91ec2fb5c6c54731f6c8973e6f84
Aksh Kaushik <aksh08022006@gmail.com>
2026-03-20 19:19:08 +0530

    fix: correct environment import path

--- frontend/src/app/core/components/login/login.component.ts
+++ frontend/src/app/core/components/login/login.component.ts
@@ -11,7 +11,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTabsModule } from '@angular/material/tabs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../../environments/environment';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-login',
